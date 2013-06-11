cd01caa6483c5bece7518ca15c8309e6d4c1e4c6
Summer Ding <zqding@hotmail.com>
2013-06-11 18:53:04 +0800

    ADDED: Added app treemenus to the project. treemenus is used to organize the menu dynamically.

--- source/BlackBam/settings.py
+++ source/BlackBam/settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'treemenus',
     'rest_framework',
 	'BlackBam.projects',
     'BlackBam.labors',
