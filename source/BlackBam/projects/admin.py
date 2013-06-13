d73ce5ffe2a7c30b8d4316bd64409212c2b217f8
Summer Ding <zqding@hotmail.com>
2013-06-13 22:11:40 +0800

    UPDATED: Use verbose_name to make items displayed in Chinese in admin

--- source/BlackBam/projects/admin.py
+++ source/BlackBam/projects/admin.py
@@ -3,4 +3,10 @@
 from django.contrib import admin
 from BlackBam.projects.models import Project
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'year')
+    search_fields = ('name', 'address', 'year')
+    list_filter = ('year',)
+    ordering = ('year',)
+
+admin.site.register(Project, ProjectAdmin)
--- source/BlackBam/projects/models.py
+++ source/BlackBam/projects/models.py
@@ -4,11 +4,15 @@ from django.db import models
 
 #项目
 class Project(models.Model):
-    name = models.CharField(max_length=200, blank=False)
-    address = models.CharField(max_length=200)
-    year = models.IntegerField()
+    name = models.CharField(max_length=200, blank=False, verbose_name='名称')
+    address = models.CharField(max_length=200, verbose_name='地址')
+    year = models.IntegerField(verbose_name='年份')
 
     def __unicode__(self):
-        return "Project_" + self.name
+        return self.name
+
+    class Meta:
+        verbose_name = '工程'
+        verbose_name_plural = '工程'
 
 	
